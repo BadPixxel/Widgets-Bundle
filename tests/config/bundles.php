f07a79dccd2516462d382e7288281124bd8bf8c6
BadPixxel <eshop.bpaquier@gmail.com>
2025-02-24 19:41:45 +0100

    WIP: Migrate to badpixxel domain WIP: Build V3.0

--- tests/config/bundles.php
+++ tests/config/bundles.php
@@ -16,8 +16,14 @@ return array(
     // SYMFONY CORE
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => array("all" => true),
     Symfony\Bundle\TwigBundle\TwigBundle::class => array("all" => true),
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => array("all" => true),
     Symfony\Bundle\MonologBundle\MonologBundle::class => array("all" => true),
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => array("all" => true),
+    //==============================================================================
+    // SYMFONY UX BUNDLES
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => array("all" => true),
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => array("all" => true),
+    Symfony\UX\StimulusBundle\StimulusBundle::class => array("all" => true),
+    Symfony\UX\Chartjs\ChartjsBundle::class => array("all" => true),
     //==============================================================================
     // SYMFONY DEV & DEBUG BUNDLES
     Symfony\Bundle\DebugBundle\DebugBundle::class => array("dev" => true),
@@ -35,8 +41,16 @@ return array(
     //==============================================================================
     // SONATA BUNDLES
     Sonata\BlockBundle\SonataBlockBundle::class => array("all" => true),
+    Sonata\AdminBundle\SonataAdminBundle::class => array("all" => true),
+    Sonata\Form\Bridge\Symfony\SonataFormBundle::class => array("all" => true),
+    Sonata\Twig\Bridge\Symfony\SonataTwigBundle::class => array("all" => true),
 
     //==============================================================================
     // SPLASH WIDGETS
-    Splash\Widgets\SplashWidgetsBundle::class => array("all" => true),
+    BadPixxel\Widgets\BadpixxelWidgetsBundle::class => array("all" => true),
+    BadPixxel\Widgets\Demo\WidgetsDemoBundle::class => array("all" => true),
+
+    //==============================================================================
+    // DEV & DEBUG BUNDLES
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => array("dev" => true),
 );
